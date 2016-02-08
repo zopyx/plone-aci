561fa08ce599314a1fd86a185b81d52112059edf
root <root@vmd9666.contabo.host>
2016-02-08 18:43:09 +0100

    updated

--- plone.sh
+++ plone.sh
@@ -8,8 +8,8 @@ acbuild set-exec /bin/hello
 acbuild run pip install virtualenv
 acbuild run virtualenv plone
 acbuild set-working-directory /plone
-acbuild run bin/pip install zc.buildout
-acbuild run bin/zc.buildout bootstrap
+acbuild run /plone/bin/pip install zc.buildout
+acbuild run /plone/bin/zc.buildout bootstrap
 acbuild copy buildout.cfg .
 acbuild run bin/buildout 
 acbuild write plone.aci 
