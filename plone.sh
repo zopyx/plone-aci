fbf6db219ea480f24693e5d16b2e98d8a43bd68a
root <root@vmd9666.contabo.host>
2016-02-10 13:20:00 +0100

    updated

--- plone.sh
+++ plone.sh
@@ -6,12 +6,14 @@ $BUILD set-name zopyx/plone
 $BUILD dependency add quay.io/coreos/alpine-sh
 $BUILD run -- apk update
 $BUILD run -- apk add python git  python-dev py-pip bash alpine-sdk libxml2-dev libxslt-dev  zlib-dev
+$BUILD run -- adduser -S plone
 $BUILD run ls /bin
 $BUILD run pip install virtualenv
 $BUILD run virtualenv plone
 $BUILD copy buildout.cfg /plone/buildout.cfg
 $BUILD copy build.sh /plone/build.sh
 $BUILD run /bin/bash /plone/build.sh
+$BUILD run /bin/chown -r plone /plone
 $BUILD set-exec -- /plone/bin/instance fg
 $BUILD write --overwrite=true plone.aci 
 $BUILD end
